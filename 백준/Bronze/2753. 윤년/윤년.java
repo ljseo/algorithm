2d4feca2a026a9292ca86fc6250d6d6ddfe1f9fe
이준서 <108654626+ljseo@users.noreply.github.com>
2025-06-06 11:25:33 +0900

    [Bronze V] Title: 윤년, Time: 184 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year = new Scanner(System.in).nextInt();
+        System.out.println(year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? 1 : 0);
+    }
+}
