9103b9bbfc97844b9d2ba614aeb7420e08e122b7
이준서 <108654626+ljseo@users.noreply.github.com>
2025-06-06 12:14:50 +0900

    [Silver V] Title: 단어 정렬, Time: 392 ms, Memory: 22508 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,24 +3,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+//
+//public class 단어정렬 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//
+//        Map <String, Boolean> map = new HashMap <> ();
+//        List<String> words = new ArrayList <> ();
+//
+//        for (int i = 0; i < n; i++) {
+//            String word = br.readLine();
+//            if(!map.containsKey(word)) {
+//                words.add(word);
+//                map.put(word, true);
+//            }
+//        }
+//
+//        words.sort(new Comparator<>() {
+//            @Override
+//            public int compare (String o1, String o2){
+//                if(o1.length() == o2.length()) {
+//                    return o1.compareTo(o2);
+//                }
+//                return o1.length() - o2.length();
+//            }
+//        });
+//
+//        for(String word : words) {
+//            System.out.println(word);
+//        }
+//
+//    }
+//}
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Map <String, Boolean> map = new HashMap <> ();
-        List<String> words = new ArrayList <> ();
+        String [] words = new String[n];
 
         for (int i = 0; i < n; i++) {
-            String word = br.readLine();
-            if(!map.containsKey(word)) {
-                words.add(word);
-                map.put(word, true);
-            }
+            words[i] = br.readLine();
         }
 
-        words.sort(new Comparator<>() {
+        Arrays.sort(words, new Comparator<String>() {
             @Override
             public int compare (String o1, String o2){
                 if(o1.length() == o2.length()) {
@@ -29,11 +57,12 @@ public class Main {
                 return o1.length() - o2.length();
             }
         });
-
-        for(String word : words) {
-            System.out.println(word);
+        System.out.println(words[0]);
+        for(int i = 1; i < n; i++){
+            if(!words[i].equals(words[i-1])) {
+                System.out.println(words[i]);
+            }
         }
 
     }
-
 }
