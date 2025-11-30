657cecd3024b78fd424f9911d56106c3ad081e63
이준서 <108654626+ljseo@users.noreply.github.com>
2025-12-01 07:07:55 +0900

    [level 2] Title: 교점에 별 만들기, Time: 19.70 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,76 +1,75 @@
 import java.util.*;
 
-class Point{
-    long x;
-    long y;
-    public Point(long x, long y){
-        this.x = x;
-        this.y = y;
+class Point {
+    double r;
+    double c;
+    public Point (double r, double c){
+        this.r = r;
+        this.c = c;
     }
 }
 
 
 class Solution {
     
-    int[][] line;
-    List<Point> pointList = new ArrayList<>();
-    long mnX = Long.MAX_VALUE;
-    long mnY = Long.MAX_VALUE;
-    long mxX = Long.MIN_VALUE;
-    long mxY = Long.MIN_VALUE;
+    static List<Point> pointList = new ArrayList<>();
+    static double mxR = Integer.MIN_VALUE;
+    static double mnR = Integer.MAX_VALUE;
+    
+    static double mxC = Integer.MIN_VALUE;
+    static double mnC = Integer.MAX_VALUE;
     
     public String[] solution(int[][] line) {
         
-        this.line = line;
         int len = line.length;
         for(int i = 0; i < len; i++){
-            for(int j = i; j < len; j++){
-                getPoint(i, j);
+            for(int j = i + 1; j < len; j++){
+                getPoint(line[i], line[j]);
             }
         }
         
-        int lenX = (int)(mxX - mnX + 1);
-        int lenY = (int)(mxY - mnY + 1);
-        char[][] grid = new char[lenY][lenX];
-        for(int i = 0; i < lenY; i++){
-            Arrays.fill(grid[i], '.');            
-        }
+        Collections.sort(pointList, (o1, o2) -> {
+            if(o1.c == o2.c) return (int)o1.r - (int)o2.r;
+            return (int)o2.c - (int)o1.c;
+        });
+    
+        int len1 = Math.abs((int)mxC - (int)mnC + 1);
+        int len2 = Math.abs((int)mxR - (int)mnR + 1);
+        char grid[][] = new char[len1][len2];
         
+        for(int i = 0; i < grid.length; i++) Arrays.fill(grid[i], '.');
         for(Point p : pointList){
-            int x = (int)(p.x - mnX);
-            int y = (int)(mxY - p.y);
-            grid[y][x] = '*';
+            int r = (int)p.r;
+            int c = (int)p.c;
+            r -= mnR;
+            c = (int)mxC - c;
+            grid[c][r] = '*';
         }
         
-        String[] answer = new String[lenY];
-        for(int i = 0; i < lenY; i++){
-            answer[i] = String.valueOf(grid[i]);
+        String[] answer = new String[len1];
+        for(int i = grid.length-1; i >= 0; i--){
+            answer[i] = new String(grid[i]);
         }
         return answer;
     }
-    
-    void getPoint(int i, int j){
-        long a1 = line[i][0];
-        long b1 = line[i][1];
-        long c1 = line[i][2];
-        long a2 = line[j][0];
-        long b2 = line[j][1];
-        long c2 = line[j][2];
-        
-        double d = (a1 * b2 - a2 * b1);
+    static void getPoint(int[] arr1, int[] arr2){
+        long a1 = arr1[0];
+        long b1 = arr1[1];
+        long c1 = arr1[2];
+        long a2 = arr2[0];
+        long b2 = arr2[1];
+        long c2 = arr2[2];
+        long d = a1 * b2 - b1 * a2;
         if(d == 0) return;
-        double x = (c2 * b1 - c1 * b2) / d;
-        double y = (c1 * a2 - c2 * a1) / d;
+        double x = (double)(c2 * b1 - c1 * b2) / (a1 * b2 - a2 * b1);
+        double y = (double)(c2 * a1 - c1 * a2) / (b1 * a2 - b2 * a1);
+        
         if(x % 1 == 0 && y % 1 == 0){
-            updateMaxMin((long)x,(long)y);
-            pointList.add(new Point((long)x, (long)y));
+            mxR = Math.max(mxR, x);
+            mnR = Math.min(mnR, x);
+            mxC = Math.max(mxC, y);
+            mnC = Math.min(mnC, y);
+            pointList.add(new Point(x,y));
         }
-    }
-    
-    void updateMaxMin(long x, long y){
-        mxX = Math.max(mxX, x);
-        mxY = Math.max(mxY, y);
-        mnX = Math.min(mnX, x);
-        mnY = Math.min(mnY, y);
     }
 }
