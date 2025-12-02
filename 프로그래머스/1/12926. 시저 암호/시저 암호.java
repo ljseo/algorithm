2d05c8c1df4509f1c8f907fdec411f31fe353de1
이준서 <108654626+ljseo@users.noreply.github.com>
2025-12-02 10:16:25 +0900

    [level 1] Title: 시저 암호, Time: 2.70 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,29 +1,17 @@
 class Solution {
-    
-    int n;
     public String solution(String s, int n) {
-        this.n = n;
-        char[] chars = s.toCharArray();
-        for(int i = 0; i < chars.length; i++){
-            chars[i] = push(chars[i]);
+        
+        StringBuilder builder = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(!Character.isAlphabetic(c)) {
+                builder.append(c);
+                continue;
+            }
+            char offset = Character.isUpperCase(c) ? 'A' : 'a';
+            char len = ('z' - 'a' + 1);
+            builder.append((char)((c - offset + n) % len + offset));
         }
-        return String.valueOf(chars);
-    }
-    
-    char push(char c){
-        if(isLowerCase(c)){
-            return (char)((c - 'a' + n) % ('z' - 'a' + 1) + 'a');
-        }
-        else if(isUpperCase(c)){
-            return (char)((c - 'A' + n) % ('Z' - 'A' + 1) + 'A');
-        }
-        return c;
-    }
-    
-    boolean isLowerCase(char c){
-        return 'a' <= c && c <= 'z';
-    }
-    boolean isUpperCase(char c){
-        return 'A' <= c && c <= 'Z';
+        
+        return builder.toString();
     }
 }
